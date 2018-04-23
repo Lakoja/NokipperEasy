ca5b946ee8d308275562879d5734ee86a79f6275
Ulrich K <github@aquilian.de>
2018-04-23 21:35:42 +0200

    Minimum (working) copy of Nokipper

---
+++ LineStepper.h
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2018 Lakoja on github.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __LINE_STEPPER_H__
+#define __LINE_STEPPER_H__
+
+class LineStepper
+{
+private:
+  const double MIN_FREQUENCY = 1.0;
+  const double MAX_FREQUENCY = 20000.0;
+  const uint16_t MIN_LEVEL_MICROS = 2;
+  
+  uint8_t stepPin;
+  uint32_t lastDriveMicros = 0;
+  double currentLiveFrequency = 0;
+  uint32_t currentLiveFrequencyDoubledMicros = 0;
+  double currentDesiredFrequency = 0;
+  uint32_t lastLevelChangeMicros = 0;
+  bool levelIsHigh = false;
+  uint32_t stepCount = 0;
+
+public:
+
+  void setup(uint8_t pin)
+  {
+    stepPin = pin;
+    outputPin(stepPin);
+  }
+
+  void drive()
+  {
+    uint32_t nowMicros = esp_timer_get_time();
+
+    if (nowMicros - lastDriveMicros >= 50) {
+      bool frequencyChange = currentDesiredFrequency != currentLiveFrequency;
+
+      if (frequencyChange) {
+        currentLiveFrequency = currentDesiredFrequency;
+        if (currentLiveFrequency != 0) {
+          currentLiveFrequencyDoubledMicros = getFrequencyDoubledMicros(currentLiveFrequency);
+        }
+      }
+
+      if (currentLiveFrequency != 0) {
+        uint32_t nextDesiredMicros = lastLevelChangeMicros + currentLiveFrequencyDoubledMicros;
+
+        if (nowMicros >= nextDesiredMicros) {
+          changeLevel();
+          lastLevelChangeMicros = nowMicros;
+        }
+        
+      } else if (levelIsHigh) {
+        changeLevel();
+      }
+      
+      lastDriveMicros = nowMicros;
+    }
+  }
+
+  void setFrequency(double freq)
+  {
+    if (freq != 0) {
+      freq = max(abs(freq), MIN_FREQUENCY);
+    }
+
+    if (freq > MAX_FREQUENCY) {
+      Serial.println("\n!!!!!!!!!!!! Frequency too high!!!!! "+String(freq));
+    }
+
+    currentDesiredFrequency = freq;
+  }
+
+  int32_t getCurrentSteps(bool reset)
+  {
+    int32_t current = stepCount;
+    if (reset) {  
+      stepCount = 0;
+    }
+
+    return current;
+  }
+
+private:
+  void outputPin(int num)
+  {
+    digitalWrite(num, LOW);
+    pinMode(num, OUTPUT);
+  }
+
+  void changeLevel()
+  {
+    digitalWrite(stepPin, levelIsHigh ? LOW : HIGH);
+    levelIsHigh = !levelIsHigh;
+    if (levelIsHigh) {
+      ++stepCount;
+    }
+  }
+
+  uint32_t getFrequencyDoubledMicros(double frequency)
+  {
+    if (frequency == 0) {
+      Serial.println("\n!!!!!!!!!!!! Division by ZERO frequency!!!!!");
+      return 1000000;
+    }
+    
+    return round(1000000.0 / (frequency * 2));
+  }
+};
+
+#endif
---
+++ NokipperEasy.ino
@@ -0,0 +1,130 @@
+#include "LineStepper.h"
+#include "GY25.h"
+#include <PID_v1.h>
+
+const int MOTOR_DIR = 14;
+const int MOTOR_STEP = 27;
+const int MOTOR_SLEEP = 26;
+
+const int MOTOR2_SLEEP = 32;
+const int MOTOR2_STEP = 33;
+const int MOTOR2_DIR = 25;
+
+const uint16_t MOTOR_MAX_RPM = 70; // for steppers this can be higher (and weaker)
+const uint16_t MOTOR_RESOLUTION = 800; // steps per rotation; this assumes a sub-step sampling (drv8834) of 4
+
+bool dirIsHigh = false;
+uint32_t systemStartTime = 0;
+uint32_t lastOutputTime = 0;
+int32_t stepCount = 0;
+double steppedFrequency = 0;
+
+LineStepper stepper;
+LineStepper stepper2;
+
+GY25 gy25;
+
+double pidSetpoint = 90, pidInput = 90, pidOutput;
+double Kp=0.35, Ki=0.0, Kd=0.001;
+PID myPID(&pidInput, &pidOutput, &pidSetpoint, Kp, Ki, Kd, DIRECT);
+
+
+void outputPin(int num)
+{
+  digitalWrite(num, LOW);
+  pinMode(num, OUTPUT);
+}
+
+// TODO this works?
+#define SIGN(x) x >= 0 ? 1 : -1
+
+static inline double sign(double x) {
+  return x >= 0 ? 1 : -1;
+}
+
+static double inline clip(double z, double max)
+{
+  if (abs(z) <= max)
+    return z;
+
+  return max * sign(z);
+}
+  
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Balance start");
+
+  outputPin(MOTOR_DIR);
+  outputPin(MOTOR_SLEEP);
+
+  outputPin(MOTOR2_DIR);
+  outputPin(MOTOR2_SLEEP);
+
+  stepper.setup(MOTOR_STEP);
+  stepper2.setup(MOTOR2_STEP);
+  
+  gy25.init();
+
+  myPID.SetOutputLimits(-1, 1);
+  myPID.SetSampleTime(10);
+
+  systemStartTime = millis();
+
+  myPID.SetMode(AUTOMATIC);
+  
+  digitalWrite(MOTOR_SLEEP, HIGH);
+  digitalWrite(MOTOR2_SLEEP, HIGH);
+  delay(1);
+
+  Serial.println("Setup done");
+}
+
+void loop()
+{
+  bool hasNewOri = gy25.drive();
+
+  if (hasNewOri) {
+    float ori = gy25.getRoll() +8;
+
+    if (ori > 45 && ori < 135) {
+      stepCount += stepper.getCurrentSteps(true) * sign(steppedFrequency);
+      double off = stepCount / (double)MOTOR_RESOLUTION;
+      off = clip(off, 2);
+      pidInput = ori - off;
+    
+      bool hasNewPid = myPID.Compute();
+
+      uint32_t now = millis();
+      if (now - lastOutputTime > 300) {
+      //if (abs(pidOutput) > 0 && abs(pidOutput) < 0.0001) {
+        Serial.println("ori "+String(ori)+" off "+String(off)+" (steps "+String(stepCount)+") > input "+String(pidInput)+" > output "+String(pidOutput));
+        lastOutputTime = now;
+      }
+
+      if (hasNewPid) {
+        double frequency = pidOutput * MOTOR_MAX_RPM / 60.0 * MOTOR_RESOLUTION;
+
+        if (frequency != steppedFrequency) {
+          digitalWrite(MOTOR_DIR, frequency >= 0 ? LOW : HIGH);
+          digitalWrite(MOTOR2_DIR, frequency < 0 ? LOW : HIGH); // inverted
+
+          delayMicroseconds(10);
+  
+          stepper.setFrequency(frequency);
+          stepper2.setFrequency(frequency);
+
+          steppedFrequency = frequency;
+        }
+      }
+    } else {
+      stepper.setFrequency(0);
+      stepper2.setFrequency(0);
+      steppedFrequency = 0;
+    }
+  }
+
+  stepper.drive();
+  stepper2.drive();
+  
+  delayMicroseconds(10);
+}
